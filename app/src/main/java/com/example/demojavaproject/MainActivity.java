4e28ee28855b16b83b7054bac26ddf348635da76
ZarifKhakimov <98202430+ZarifKhakimov@users.noreply.github.com>
2022-04-18 15:00:07 +0500

    Initial commit

--- app/src/main/java/com/example/demojavaproject/MainActivity.java
+++ app/src/main/java/com/example/demojavaproject/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.demojavaproject;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -16,15 +14,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.demojavaproject.model.Member;
 import com.example.demojavaproject.model.User;
 
-import java.lang.reflect.Member;
+
 
 public class MainActivity extends AppCompatActivity {
     static final String TAG = MainActivity.class.toString();
 
-    int LAUNCH_DETAIL = 22;
-    TextView tv_home;
+    TextView tv_home;//int LAUNCH_DETAIL = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityResultLauncher<Intent> detailLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>(){
+            new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if(result.getResultCode() == Activity.RESULT_OK){
-            Intent data = result.getData();
-            String text = data.getStringExtra("result");
-            Log.d(TAG, "text");
-            tv_home.setText(text);
-        }
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        User user = new User(14, "Zarifjon");
+                        Log.d(TAG, "user");
+                        tv_home.setText(user.toString());
+                    }
                 }
             });
 
@@ -54,20 +52,23 @@ public class MainActivity extends AppCompatActivity {
         b_detail.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                User user = new User(14, "Zarifjon");
+                User user = new User(14, "SENATOR");
                 openDetailActivity(user);
             }
         });
-        Member member = (Member)getIntent().getSerializableExtra("member");
-        Log.d(TAG, member.toString());
+       if(getIntent().hasExtra("member")){
+           Member member = (Member)getIntent().getSerializableExtra("member");
+           Log.d(TAG, member.toString());
 
-        tv_home.setText(member.toString());
+           tv_home.setText(member.toString());
+       }
     }
 
     void openDetailActivity(User user){
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("user", user);
-        setResult(Activity.RESULT_OK);
+        setResult(Activity.RESULT_OK, intent);
         detailLauncher.launch(intent);
+
     }
 }
